1e76783bb659582fb7a3c3ab9bcba44d45186a32
Aleksandra <aleksandra_miszkiel@o2.pl>
2023-06-20 18:50:26 +0200

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     viewportHeight: 1410,
     viewportWidth: 1080,
@@ -9,6 +10,7 @@ module.exports = defineConfig({
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
